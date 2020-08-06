402f156c3e3db5b12fb9981ff4a9e8ebdd81c3d6
mcclayac <anthony.mcclay@me.com>
2020-08-06 05:41:34 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,14 @@
+void main(List<String> arguments) {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print('Dart Quick Start Chapter 5');
+  print('5. Entity Relationships: Abstract Classes, Interfaces, and Exception Handling');
+  //https://learning.oreilly.com/library/view/quick-start-guide/9781484255629/A490744_1_En_5_Chapter.html
+
+  
+
+}
